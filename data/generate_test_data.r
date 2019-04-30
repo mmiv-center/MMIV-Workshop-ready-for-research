f3ce07b45bb1d23e61cb3fc11e9dbaa65cf0c780
Hauke Bartsch <HaukeBartsch@gmail.com>
2019-04-30 09:46:24 +0200

    add some basic test data for merge

---
+++ data/generate_test_data.r
@@ -0,0 +1,20 @@
+# generate some test data for a merge
+d1 = data.frame(PatientID=c("P000"), PatientName=c("P000"), StudyDate=as.Date("20190101","%Y%m%d"), Vol_Hippocampus=c(5200), Vol_Amygdala=c(1000))
+num = 500
+for (i in 1:num) {
+  date = sample(seq(as.Date('1999/01/01'), as.Date('2019/01/01'), by="day"), 1)
+  r = data.frame(PatientID=c(sprintf("P%03d", i)), PatientName=c(sprintf("P%03d", i)), StudyDate=date, Vol_Hippocampus=c(5000*runif(1)), Vol_Amygdala=c(100000*runif(1)))
+  d1 <- rbind(d1, r)
+}
+
+# generate some test data for a merge
+d2 = d1
+d2$intra_cranial_volume = d2$Vol_Hippocampus*23
+d2$Vol_Amygdala_l = d2$Vol_Amygdala/2
+d2$Vol_Hippocampus = NULL
+d2$Vol_Amygdala = NULL
+# remove some rows
+d2 = d2[-seq(100,200),]
+
+write.csv(d1, 'd1.csv')
+write.csv(d2, 'd2.csv')
